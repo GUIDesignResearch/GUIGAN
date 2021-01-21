c2658b3a011470acaef201f288ac28435167a3ee
GUIDesignResearch <50594359+GUIDesignResearch@users.noreply.github.com>
2021-01-21 10:00:22 +0800

    Add files via upload

---
+++ code/GetSubtree/get_subtree.py
@@ -0,0 +1,389 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import json,os,shutil
+import matplotlib.pyplot as plt
+from PIL import Image
+
+def get_json(cd):
+    with open(cd, encoding='utf-8') as f:
+        line = f.read()
+        d = json.loads(line)
+        f.close()    
+    a = d['activity']['root']
+    return a
+
+def get_width(bounds):
+    assert bounds[2]-bounds[0]>0
+    return bounds[2]-bounds[0]
+
+def get_height(bounds):
+    assert bounds[3]-bounds[1]>0
+    return bounds[3]-bounds[1]
+
+def get_bound_asname(bound):
+    _b = ''
+    for b in bound:
+        _b += str(b)+','
+    return _b
+
+def get_layerNum(c):
+    return int(c.split('_')[-1].split('-')[0])
+
+def get_className(c):
+    return c.split('.')[-1]
+
+def get_father(c):
+    d = c.split('_')[-1]
+    sr = c.find(d)    
+    return c[:sr-1]       
+
+def judge_loc_height(bounds):
+    y1 = bounds[0][1]
+    y2 = bounds[0][3]
+    height = get_height(bounds[0])
+    for b in bounds[1:]:
+        if b[1]==y1 and b[3]==y2 and get_height(b)==height:
+            continue
+#            print(b,'same as',y1,y2,height)
+        else:
+            return False
+    return True    
+
+def get_subNodes_ofC1(c,Dd): # find node only belongs to c in Dd
+    Dd_ids = [d[0] for d in Dd]
+    c_children = []
+    for d in Dd_ids:
+        if c in d:
+            if d not in c_children:
+                c_children.append(d) # all children of c
+    # find node only belongs to c
+    others_subNodes = []
+    for d in c_children: #if there are other fathers of d
+        c_children1 = c_children.copy()
+        c_children1.remove(d)
+        for f in c_children1: # traversal in all Dd but d
+            if f in d:
+                if d not in others_subNodes:
+                    others_subNodes.append(d)
+    c_children = [c for c in c_children if c not in others_subNodes]
+    return c_children    
+
+'''put all tree_info of one GUI in a List'''
+def get_DOM(list_Dom, father, layer_num, last_layer='0-0'):
+    if('children' in father):
+        i = 0
+        next_layer_num = layer_num + 1
+        for child in father['children']:
+            if child is None:
+                continue            
+            c_layer = str(layer_num)+'-'+str(i)            
+            if('resource-id' in child and child is not None):
+                _id = child['resource-id']
+            else:
+                _id = ''
+            if('children' in child):
+                _children_num = len(child['children'])
+            else:
+                _children_num = 0
+            link = last_layer +'_'+c_layer
+            
+            if child['visible-to-user']==True:
+                list_Dom.append([link,child['class'],child['bounds'],_children_num,_id])            
+            i += 1
+#            print('layer:', str(c_layer), child['bounds'],'; link',str(link))
+            get_DOM(list_Dom, child, next_layer_num, link)
+
+def DeleteOverSize(_list):
+    list_ddsf_leaf4 = []
+    for c in _list:
+        w = get_width(c[2]); h = get_height(c[2])
+        if w/h>=30 or h/w>= 50:
+            print('illegal aspect ratio')
+            continue
+        elif h >= 1280 or h <5:
+            print('illegal height')
+            continue
+        elif h <= 150 and w <= 800:
+            print('too small piece')
+            continue
+        elif w <= 1000:
+            print('not wide enough')
+            continue        
+        else:
+            list_ddsf_leaf4.append(c)
+    return list_ddsf_leaf4
+
+def DeleteOverlap(_list):
+    blank_num = 0
+    blank_ls = []
+    blank_lsd = {}
+    last_loc = 0
+    m = 0
+    out = _list.copy()
+    list_wb = []
+    if len(_list)>0:
+        list_wb.append(_list[0][0])
+    list_wb_del = []
+    # record info of blank，order by(1)top left coordinate,(2)height
+    for l in _list:
+        loc1 = int(l[2][1])  # Top coordinates
+        loc2 = int(l[2][-1]) # Bottom coordinates
+        print('m: ', m, 'l: ', l[0])
+        print('m: ', m, 'top loc1: ', loc1, 'bottom loc2: ', loc2)        
+        if m > 0: 
+            blank_num += 1
+            print('loc: ', last_loc, loc1)
+            bk = loc1 - last_loc
+            print('blank: ', bk)
+            if bk < 0:
+                bk = -1
+                out.remove(l)
+                list_wb_del.append(l[0])
+                if loc2>last_loc: # 2.
+                    last_loc = loc2
+            else:
+                if bk != 0:
+                    list_wb.append('bk_'+str(bk))
+                last_loc = loc2
+            if bk not in blank_ls:
+                blank_ls.append(bk)
+                blank_lsd[bk] = 1
+            else:
+                blank_lsd[bk] += 1
+            list_wb.append(l[0])
+        else:
+            last_loc = loc2
+        m += 1
+    return out, list_wb, list_wb_del
+
+def get_component_byjson(json_file):
+    
+    '''read json file to get tree info'''
+    a0 = get_json(json_file)    
+    list_Dom = []
+    get_DOM(list_Dom,a0,1)
+    
+    '''del subtrees(sts) with duplicate bounds and leave one'''
+    same_bounds_dict = {}
+    same_bounds_list = []
+    for c in list_Dom:
+        if c[2] not in same_bounds_list:# new bounds
+            _bounds = c[2]
+            same_bounds_list.append(_bounds)
+            same_bounds_dict[get_bound_asname(_bounds)] = [c[0]]
+        else:                           # old bounds
+            same_bounds_dict[get_bound_asname(c[2])].append(c[0])
+            
+    same_bounds_dict1 = same_bounds_dict.copy()
+    for (k,v) in same_bounds_dict.items():
+        if len(v) == 1:
+            same_bounds_dict1.pop(k,v)
+    
+    list_for_del = []
+    for (k,v) in same_bounds_dict1.items():
+        last_id_len = np.max([len(vs) for vs in v])
+        last_ids = [c for c in v if len(c)==last_id_len]
+        for c in v: # leave the last samebounds
+            if c !=last_ids[-1]:
+                list_for_del.append(c)
+
+    # delete
+    list_Dom_del_st = [c for c in list_Dom if c[0] not in list_for_del]
+    
+    list_Dom_del_st_fit = [c for c in list_Dom_del_st if c[2][2]>c[2][0] and c[2][3]>c[2][1] and c[0]!='0-0_1-0']
+    list_ddsf_leaf = [c for c in list_Dom_del_st_fit if c[3]==0]
+    list_ddsf_node = [c for c in list_Dom_del_st_fit if c[3]!=0]
+    
+    #------------------------------------------------------------------------------------#    
+    # Find the node with the same father
+    # (1)by bounds (2) remove the covered ones, 
+    # (3)remove all subtrees in the same row with the same length and height
+
+    node_fs_l = [] # Constructing the L, D of the fathers node
+    node_fs_d = {} # key:father, value:children(nodes in list_ddsf_node)
+    for c in list_ddsf_node:
+        if get_father(c[0]) not in node_fs_l:
+            node_fs_l.append(get_father(c[0]))
+            node_fs_d[get_father(c[0])] = [c[0]]
+        else:
+            node_fs_d[get_father(c[0])].append(c[0])
+    
+    c_for_del = [] # Nodes whose width exceeds the limit need to be pruned (delete all their children subtrees)
+    width_r = 0.95
+    w_limit = 1440*width_r
+    for (k,v) in node_fs_d.items():
+        if len(v)>2:
+            for c in v:
+                if get_width([i[2] for i in list_ddsf_node if i[0]==c][0]) >= w_limit:
+                    c_for_del.append(c)
+    
+    '''Delete children st beyond limited width'''
+    list_ddsfcutfc_cb = list_ddsf_node.copy()
+    for c in list_ddsf_node:
+        for d in c_for_del:
+            if d in c[0] and d!=c[0]:
+                if c in list_ddsfcutfc_cb:                    
+                    list_ddsfcutfc_cb.remove(c)
+#    print('Delete',del_node_num,'nodes from list_ddsf_node\n') # (1)Finished    
+    list_ddsfcutfccb_ch = list_ddsfcutfc_cb.copy()
+    
+    #------------------------------------------------------------------------------------#    
+    '''find sts with same loc_height'''
+    node1_fs_l = [] 
+    node1_fs_d = {} 
+    for c in list_ddsfcutfccb_ch:
+        if get_father(c[0]) not in node1_fs_l:
+            node1_fs_l.append(get_father(c[0]))
+            node1_fs_d[get_father(c[0])] = [(c[0],c[2])]
+        else:
+            node1_fs_d[get_father(c[0])].append((c[0],c[2]))
+    
+    list_for_del_slh = []
+    # On the same line, y1 is the same, y2 is the same, and so is height
+    for (k,v) in node1_fs_d.items():
+        if len(v)>2: 
+            _bounds = [c[1] for c in v]
+            # For nodes with more than two brothers, judge whether they are in the same line and 
+            # of the same height. If yes, cut them off (their children should also be cut off)
+            if judge_loc_height(_bounds)==True:
+                for c in v:
+                    if c[0] not in list_for_del_slh:
+                        list_for_del_slh.append(c[0])
+                    for d in list_ddsfcutfccb_ch:
+                        if c[0] in d[0] and c[0] != d[0]:
+                            if d[0] not in list_for_del_slh:
+                                list_for_del_slh.append(d[0])
+                    
+    list_ddsfcutfccbch_cslh=[c for c in list_ddsfcutfccb_ch if c[0] not in list_for_del_slh] # (3)Finished
+    
+    #cslh Delete c of the same height and record the blanks
+    '''del sts with the same height in one line'''
+    l_delete = []
+    for c in list_ddsfcutfccbch_cslh:
+        c_height = get_height(c[2])
+        l_e_c = list_ddsfcutfccbch_cslh.copy()
+        l_e_c.remove(c)
+        if l_e_c != []:
+            c_children = get_subNodes_ofC1(c[0],l_e_c)
+            print('c_children',c_children)
+            for d in c_children:
+                d_bound = [f[2] for f in list_ddsfcutfccbch_cslh if f[0]==d]
+                d_height = get_height(d_bound[0]) 
+                print('d:',d,',height:',d_height)
+                if c_height == d_height:
+                    if d not in l_delete:
+                        l_delete.append(d)
+                        for d1 in list_ddsfcutfccb_ch:
+                            if d in d1[0] and d != d1[0]:
+                                print('delete d',d,'children',d1[0])
+                                if d1[0] not in l_delete:
+                                    l_delete.append(d1[0])
+        else:
+            print('c_children is []')
+    list_ddsfcutfccbch_cslh1 = [c for c in list_ddsfcutfccbch_cslh if c[0] not in l_delete]
+    
+    print('\ndelete subtree not fit:')
+    list_ddsfcutfccbch_cslh_cutlwR2 = DeleteOverSize(list_ddsfcutfccbch_cslh1) #(4) Finished  
+    
+    #------------------------------------------------------------------------------------#    
+    '''sort subtrees by coordinates and delete overlap'''
+    list_ddsfcutfccbch_cslh_cutlwR2 = sorted(list_ddsfcutfccbch_cslh_cutlwR2,key=lambda x: (int(x[2][1]), 1/get_height(x[2])))
+    list_ddsfcutfccbch_cslh_cutlwR3,_,_ = DeleteOverlap(list_ddsfcutfccbch_cslh_cutlwR2)
+    
+    #------------------------------------------------------------------------------------#   
+    '''add leafnodes(fit) to subtrees'''            
+    list_ddsf_leaf0 = [] 
+    for c in list_ddsf_leaf:
+        for f in list_ddsfcutfccbch_cslh_cutlwR3:
+            if f[0] in c[0]:
+                if c not in list_ddsf_leaf0:
+                    list_ddsf_leaf0.append(c)                    
+
+    list_ddsf_leaf2 = [c for c in list_ddsf_leaf if c[0] not in [d[0] for d in list_ddsf_leaf0]]
+
+    list_ddsf_leaf3 = [c for c in list_ddsf_leaf2 if not (c[0] in ['0-0_1-1', '0-0_1-2'] and c in list_ddsf_leaf2)]
+    
+    list_ddsf_leaf4 = DeleteOverSize(list_ddsf_leaf3)
+    
+    '''sort subtrees by coordinates and delete overlap'''    
+    list_ddsfcutfccbch_cslh_cutlwR4 = list_ddsfcutfccbch_cslh_cutlwR3+list_ddsf_leaf4
+    list_ddsfcutfccbch_cslh_cutlwR4 = sorted(list_ddsfcutfccbch_cslh_cutlwR4,key=lambda x: (int(x[2][1]), 1/get_height(x[2])))
+        
+    # The second time with leaf_node
+    print('\nsecond sort and sel blank')
+    list_ddsfcutfccbch_cslh_cutlwR5,list_ddsfcutfccbch_cslh_cutlwR2_wb,list_ddsfcutfccbch_cslh_cutlwR2_wb_del = DeleteOverlap(list_ddsfcutfccbch_cslh_cutlwR4)        
+    list_ddsfcutfccbch_cslh_cutlwR2_wb1 = [c for c in list_ddsfcutfccbch_cslh_cutlwR2_wb if c not in list_ddsfcutfccbch_cslh_cutlwR2_wb_del]
+    list_ddsfcutfccbch_cslh_cutlwR3 = list_ddsfcutfccbch_cslh_cutlwR5
+    #------------------------------------------------------------------------------------#         
+    ''''del st in same father with partially covered'''
+    c_for_del = [c for c in c_for_del if c in [d[0] for d in list_ddsfcutfccbch_cslh_cutlwR3]]
+    c_for_del_heights = [(c,get_height([i[2] for i in list_ddsfcutfccbch_cslh_cutlwR3 if i[0]==c][0])) for c in c_for_del]
+    node_heght_l = []
+    node_heght_d = {}
+    for c in c_for_del_heights:
+        if c[1] not in node_heght_l:
+            node_heght_l.append(c[1])
+            node_heght_d[c[1]] = [c[0]]
+        else:
+            node_heght_d[c[1]].append(c[0])
+    del_dif_height = []
+    for (k,v) in node_heght_d.items():
+        if len(v) == 1:
+            del_dif_height.append(v[0])
+            for d in list_ddsfcutfccbch_cslh_cutlwR3:
+                if v[0] in d[0] and v[0] != d[0]:
+                    del_dif_height.append(d[0]) # del their children
+    
+    list_ddsfcutfccb_ch1 = [c for c in list_ddsfcutfccbch_cslh_cutlwR3 if c[0] not in del_dif_height] # (2)Finished
+
+    #------------------------------------------------------------------------------------# 
+    '''save_st_with_blank file'''
+    list_ddsfcutfccbch_cslh_cutlwR3_wb = list_ddsfcutfccbch_cslh_cutlwR2_wb1.copy()
+    for c in list_ddsfcutfccbch_cslh_cutlwR2_wb1:
+        if c.split('_')[0] != 'bk':
+            if c not in [d[0] for d in list_ddsfcutfccb_ch1]:
+                list_ddsfcutfccbch_cslh_cutlwR3_wb.remove(c)
+
+    #------------------------------------------------------------------------------------#     
+    '''delete st without enough area'''
+    S0 = 1440 * 2560 / 2 # half area of GUI
+    S = 0
+    for x in list_ddsfcutfccb_ch1:
+        S += get_width(x[2]) * get_height(x[2])
+    if S <= S0:
+        print('square unsatisfy')
+        list_ddsfcutfccb_ch1 = []
+    else:
+        print('square satisfy')
+    
+    return list_ddsfcutfccb_ch1,list_ddsf_leaf0,list_ddsfcutfccbch_cslh_cutlwR3_wb
+
+def get_resized_bounds(block,rate = 8/3):
+    resized_block = []
+    for i in range(len(block)):
+        resized_block.append(int(block[i])/ rate)
+    return resized_block
+
+def compressImage(srcPath,dstPath, width, height): 
+    for filename in os.listdir(srcPath):  
+        print('filename:',filename)
+        if not os.path.exists(dstPath):
+                os.makedirs(dstPath)
+
+        srcFile=os.path.join(srcPath,filename)
+        dstFile=os.path.join(dstPath,filename)
+        print('srcFile:',srcFile)
+        print('dstFile:',dstFile)
+
+        if os.path.isfile(srcFile):
+            if(os.path.splitext(srcFile)[1] == '.jpg' or os.path.splitext(srcFile)[1] == '.bmp' or os.path.splitext(srcFile)[1] == '.png'):
+                sImg=Image.open(srcFile)
+#                sImg = sImg.convert("L") 
+                w,h=sImg.size
+                dImg=sImg.resize((width,height),Image.ANTIALIAS)
+                dImg.save(dstFile) 
+                print(dstFile+" compressed succeeded")
+
+        if os.path.isdir(srcFile):
+            compressImage(srcFile,dstFile, width, height)
---
+++ code/GetSubtree/save_subtree_info.py
@@ -0,0 +1,253 @@
+# -*- coding: utf-8 -*-
+"""
+(1)save subtree_info in txt,with structural info，and identify each subtree
+(2)save_subtree_img in png file
+(3)del ori ui and resizing
+"""
+
+import os,shutil
+import numpy as np
+from get_subtree import get_component_byjson, get_className, get_layerNum, \
+get_width, get_height, get_resized_bounds, compressImage
+
+def get_clayer_nodes(Dom_list,layer1_ids=[]):
+    print('layer1_ids:',layer1_ids)
+    Dom_list1 = Dom_list.copy()
+    # Find all nodes in the first layer
+    Dom_listids = [c[0] for c in Dom_list1]
+    r = np.min([get_layerNum(c) for c in Dom_listids])
+    print('\nr:', r)
+    # Save all the shortest nodes in layer1_ids for the first search
+    layer1_ids += [c for c in Dom_listids if get_layerNum(c)==r and c not in layer1_ids]
+    print('layer1_ids:',layer1_ids)
+    # Delete the nodes in first layer
+    for c in Dom_list1:
+        if c[0] in layer1_ids:
+            Dom_list1.remove(c)
+    # Delete the children of the nodes from the first layer
+    NC_dict = {}
+    for c in Dom_list:
+        for f in layer1_ids:
+            if f in c[0] and f!=c[0]: #Find all the children of the first layer
+                if c[0] not in NC_dict.keys():
+                    NC_dict[f] = [c]
+                else:
+                    NC_dict[f].append(c)
+                if c in Dom_list1:
+                    Dom_list1.remove(c)
+    if len(Dom_list1)==0:
+        print('\n',layer1_ids,'no more layer1 Node')
+        return layer1_ids
+    else:
+        print('\nExtra',len(Dom_list1) ,'layer1 Node:')
+        [print(c[0]) for c in Dom_list1] 
+        _ids = get_clayer_nodes(Dom_list1,layer1_ids)
+        return _ids
+
+# Find the items that only belong to c in DL and do not belong to any other leaves in Dd
+def get_subLeafNodes_ofC(c,Dd,Dl):
+    Dd_ids = [d[0] for d in Dd]
+    Dl_ids = [l[0] for l in Dl]
+    leaf_ids_inDd = []
+    for d in Dd_ids:
+        for l in Dl_ids:
+            if d in l:
+                if l not in leaf_ids_inDd:
+                    leaf_ids_inDd.append(l)
+    leaf_ids_offDd = [l for l in Dl_ids if l not in leaf_ids_inDd]
+    c_leaf = []
+    for l in leaf_ids_offDd:
+        if c in l:
+            if l not in c_leaf:
+                c_leaf.append(l)
+    return c_leaf
+
+# Find the node that only belongs to c in Dd
+def get_subNodes_ofC(c,Dd): 
+    Dd_ids = [d[0] for d in Dd]
+    c_children = []
+    for d in Dd_ids:
+        if c in d:
+            if d not in c_children:
+                c_children.append(d) 
+    # Find subtree that only belongs to c
+    others_subNodes = []
+    for d in c_children: #  whether d has other father nodes
+        c_children1 = c_children.copy()
+        c_children1.remove(d)
+        for f in c_children1:
+            if f in d:
+                if d not in others_subNodes:
+                    others_subNodes.append(d)
+    c_children = [c for c in c_children if c not in others_subNodes]
+    return c_children
+
+def write_NtD(c,node_tree,Dd,Dl): # c is not in Dd, write the list of nodes to dict
+    #k is the node number, V is the node number of its childre only
+    # (1)Find the leaf of C (C only)
+    c_leafs= get_subLeafNodes_ofC(c,Dd,Dl)
+    Dl_out = [d for d in Dl if d[0] not in c_leafs] 
+    
+    c_childrens = get_subNodes_ofC(c,Dd)
+    Dd_out = [d for d in Dd if d[0] not in c_childrens]
+    
+    c_nodes_leaf = c_childrens + c_leafs
+    node_tree[c] = c_nodes_leaf 
+    
+    if c_childrens == []:
+        return node_tree,Dd_out,Dl_out
+    else:
+        for c1 in c_childrens:
+            node_tree1,Dd1,Dl1 = write_NtD(c1,node_tree,Dd_out,Dl_out)
+        return node_tree1,Dd1,Dl1
+
+def DtoT(c,tt,node_tree,id_class_d):
+    if c not in node_tree.keys():
+        tt+='}'
+        return tt
+    else:
+        for d in node_tree[c]:
+            tt+='{'+id_class_d[d]
+            tt = DtoT(d,tt,node_tree,id_class_d)
+        tt+='}'
+    return tt
+
+# Get the subtree structure of all nodes in DOMTree and Dom_list
+def get_aptedTree(Dom_list,node_tree,id_class_d):
+    aTree = []
+    # root 
+    if 'root' in [c[0] for c in Dom_list]:
+        tree_text = '{root'
+        tree_text = DtoT('root',tree_text,node_tree,id_class_d)
+        c_b = [c[2] for c in Dom_list if c[0]=='root'][0]
+        aTree.append(['root:',tree_text,c_b])
+    
+    # subtrees nodes
+    for c in Dom_list:
+        tree_text = '{'+ c[1]
+        tree_text = DtoT(c[0],tree_text,node_tree,id_class_d)
+        aTree.append([c[0]+':',tree_text,c[2]])
+    
+    return aTree
+
+def save_subtree_img(_list):
+    if _list != []:
+        for c in _list:
+            if get_height(c[2]) >= 1280:
+                _list.remove(c)                    
+        
+        for c in _list:        
+            resized_block = get_resized_bounds(c[2])
+            cropedIm = im.crop(resized_block)
+            save_dir = os.path.join(dst_dir,str(ui_id))
+            if(not os.path.exists(save_dir)):
+                os.makedirs(save_dir)
+            shutil.copy(ui_dir, save_dir)
+            save_subtree_dir = os.path.join(save_dir,c[0])
+            cropedIm.save(save_subtree_dir+'.png',"png")
+
+'''save subtree info in .txt'''
+def save_tree_process(jsns_dir,ui_id,tree_dir,db_app_dir):
+    cd0  = os.path.join(jsns_dir,ui_id+'.json')
+    
+    Dom_list,leaf_list,st_bank = get_component_byjson(cd0)
+    
+    save_subtree_img(Dom_list)
+    
+    if Dom_list != []:                
+        if Dom_list==[]:
+            print(ui_id, 'Dom_list is Empt!')
+        else:
+            print('Dom_list.len:', len(Dom_list))
+            print('st_bank.len: ', len(st_bank))
+            Dom_list = [(c[0],get_className(c[1]),c[2]) for c in Dom_list]
+            leaf_list = [(c[0],get_className(c[1]),c[2]) for c in leaf_list]
+            Dom_allnodes = Dom_list+leaf_list  
+            
+            node_tree = {}
+            dynamic_Dom_list = Dom_list.copy()
+            dynamic_leaf_list = leaf_list.copy()
+            
+            print('layer',len(node_tree),'root')
+            print('\ndynamic_Dom_list', dynamic_Dom_list)
+            layer1_ids = get_clayer_nodes(dynamic_Dom_list,[]) # from the first layer
+            dynamic_Dom_list = [c for c in dynamic_Dom_list if c[0] not in layer1_ids]
+            node_tree['root'] = layer1_ids
+            print('\n\n',len(layer1_ids),'first layers!')
+            for c in layer1_ids: # from the first layer to the next..     
+                node_tree,dynamic_Dom_list,dynamic_leaf_list = write_NtD(c,node_tree,dynamic_Dom_list,dynamic_leaf_list)
+            
+            node_tree1 = node_tree.copy()
+            for (k,v) in node_tree.items():
+                if k not in [c[0] for c in Dom_list]:
+                    node_tree1.pop(k)
+                
+        # use node_tree to build st structure like: tree1=Tree.from_text('{A{B{M}{N}{F}}{C}}')
+            id_class_d = {} # Build a dict that stores all node id-class
+            for c in Dom_allnodes:
+                id_class_d[c[0]] = c[1]
+            
+            aTrees = get_aptedTree(Dom_list,node_tree1,id_class_d)
+            ui_tree_dir = os.path.join(tree_dir,ui_id+'.txt')            
+            
+            f1 = open(ui_tree_dir,'w',encoding='utf-8') # width, height
+            [f1.write(t[0]+t[1]+':'+str(get_width(t[2]))+'_'+str(get_height(t[2]))+':'+str(t[2])[1:-1]+',\n') for t in aTrees] # 再写入起始坐标   
+            f1.write(';\n') 
+            for (k,v) in node_tree1.items():
+                f1.write(str(k)+' '+str(v)+'\n')
+            f1.close()
+        # save st_bank_in
+        ui_db_dir = os.path.join(db_app_dir,ui_id+'.txt')
+        f2 = open(ui_db_dir,'w',encoding='utf-8')
+        [f2.write(c+',\n') for c in st_bank]
+
+def remove_empty_f(apps_dir):
+    for app in os.listdir(apps_dir):
+        app_dir = os.path.join(apps_dir,app)
+        if not os.listdir(app_dir): # empty folder
+            print('Empty',app_dir) 
+            os.rmdir(app_dir)
+            
+#-------------------main start-----------------------------------
+# save_tree_process(jsns_dir,ui_id,tree_dir):
+
+if __name__ == "__main__":   
+    jsns_dir = r'.\Rico\combined' # json_dir
+    cd = r'.\P_app_resize_sub' # GUI_dir
+    dt = r'.\aTrees_dict_app' # ourput_dir
+    db = r'.\st_bank_app' #   # save id_list with blank
+    dsts_dir = r'.\p_app_resize_Td_sts' # save subtree imgs
+    
+    for app in os.listdir(cd):
+        app_dir = os.path.join(cd,app)
+        dt_app_dir = os.path.join(dt,app)
+        db_app_dir = os.path.join(db,app)
+        dst_dir = os.path.join(dsts_dir,app)
+        if not os.path.exists(dt_app_dir):
+            os.makedirs(dt_app_dir)
+        if not os.path.exists(db_app_dir):
+            os.makedirs(db_app_dir)
+        for im in os.listdir(app_dir):
+            img_dir = os.path.join(app_dir,im)
+            ui_id = os.path.splitext(im)[0]
+            
+            save_tree_process(jsns_dir,ui_id,dt_app_dir,db_app_dir)
+            
+            ori_img = os.path.join(os.path.join(app_dir,im),im+'.jpg')
+            if os.path.exists(ori_img):
+                os.remove(ori_img)
+            else:
+                print('no ori_img:', ori_img)
+            
+    remove_empty_f(dt)
+    remove_empty_f(db)
+    
+    resized_dir = r'.\p_app_resize_Td_sts_c_resized'
+    compressImage(dsts_dir,resized_dir,512,256)
+    
+    
+    
+    
+    
+    
+            
